ccdd25c0d8a09abaf422643f004a74e2b41ea19b
kelin-xycs <kelin_xycs@163.com>
2019-04-28 15:11:55 +0800

    Signed-off-by: kelin-xycs <kelin_xycs@163.com>

--- PicturePlayer/JsCall/JsEvent.cs
+++ PicturePlayer/JsCall/JsEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.IO;
 
@@ -20,14 +21,22 @@ namespace PicturePlayer.JsCall
             if (string.IsNullOrWhiteSpace(path))
                 throw new Exception("path 不能为 空 。");
 
-            string[] urlList = Directory.GetFiles(path);//, "(*.bmp|*.jpg|*.jpeg|*.gif|*.png)");
+            string[] urlList = Directory.GetFiles(path);
+
+            string url;
+            string ext;
+            List<string> urlList2 = new List<string>();
 
             for (int i=0; i<urlList.Length; i++)
             {
-                urlList[i] = "http://" + ToLocalFileUrl(urlList[i]);
+                url = urlList[i];
+                ext = Path.GetExtension(url);
+
+                if (ext == ".bmp" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif" || ext == ".png")
+                    urlList2.Add("http://" + ToLocalFileUrl(url));
             }
 
-            return ToJson(urlList);
+            return ToJson(urlList2);
         }
 
         private string ToLocalFileUrl(string path)
@@ -35,16 +44,16 @@ namespace PicturePlayer.JsCall
             return "localfile/" + path.Replace(":", string.Empty).Replace("\\", "/");
         }
 
-        private string ToJson(string[] arr)
+        private string ToJson(List<string> list)
         {
             StringBuilder sb = new StringBuilder();
 
             sb.Append("[");
 
-            for (int i=0; i<arr.Length; i++)
+            for (int i=0; i<list.Count; i++)
             {
                 sb.Append("'");
-                sb.Append(arr[i]);
+                sb.Append(list[i]);
                 sb.Append("',");
             }
 
